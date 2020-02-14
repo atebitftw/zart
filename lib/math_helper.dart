06fbd7c3fa51f6e3542383cc40a70fcbfdeb4ec4
John <prujohn@gmail.com>
2020-02-13 16:55:56 -0800

    added some new classes

---
+++ lib/math_helper.dart
@@ -0,0 +1,33 @@
+import 'package:zart/game_exception.dart';
+
+/// Static helper functions for common number/math needs.
+class MathHelper {
+  /// Takes any Dart [val] between -32768 & 32767 and makes a zmachine-readable
+  /// 16-bit signed 'word' from it.
+  ///
+  /// ### Z-Machine Spec Reference
+  /// 2.2
+  static int dartSignedIntTo16BitSigned(int val) {
+    if (val < -32768 || val > 32767) {
+      throw GameException("Signed 16-bit int is out of range: $val");
+    }
+
+    if (val > -1) return val;
+
+    return 65536 - val.abs();
+  }
+
+  /// Converts a game 16-bit 'word' [val] into a signed Dart int.
+  ///
+  /// ### Z-Machine Spec Reference
+  /// ref(2.2)
+  static int toSigned(int val) {
+    //if (val == 0) return val;
+
+    // game 16-bit word is always positive number to Dart
+    assert(val >= 0);
+
+    // convert to signed if 16-bit MSB is set
+    return (val & 0x8000) == 0x8000 ? -(65536 - val) : val;
+  }
+}
---
+++ lib/z_char.dart
@@ -0,0 +1,19 @@
+import 'package:zart/binary_helper.dart';
+
+class ZChar {
+  final int _word;
+  final int z1;
+  final int z2;
+  final int z3;
+
+  ZChar(this._word) :// ((word >> 15) & 1) == 1,
+        z3 = BinaryHelper.bottomBits(_word, 5),
+        z2 = BinaryHelper.bottomBits(_word >> 5, 5),
+        z1 = BinaryHelper.bottomBits(_word >> 10, 5) {
+    // print('${word.toRadixString(2)}');
+  }
+
+  bool get terminatorSet => BinaryHelper.isSet(_word, 15);
+
+  List<int> toCollection() => [z1, z2, z3];
+}
