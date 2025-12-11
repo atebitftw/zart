87591499b06ed01560741f3c0cc5893b47747013
prujohn <prujohn@gmail.com>
2025-12-10 21:54:00 -0800

    formatting

--- test/common/property_defaults_test.dart
+++ test/common/property_defaults_test.dart
@@ -23,7 +23,11 @@ void main() {
   group('GameObject Property Defaults (Version Dependent)', () {
     test('V3 (Minizork) - Max 31 properties', () {
       loadGame('assets/games/minizork.z3');
-      expect(Z.engine.version, equals(ZMachineVersions.v3), reason: 'Should be Version 3');
+      expect(
+        Z.engine.version,
+        equals(ZMachineVersions.v3),
+        reason: 'Should be Version 3',
+      );
 
       // Should work for 1-31
       // Property defaults table is readable
@@ -40,7 +44,11 @@ void main() {
 
     test('V5 (Adventureland) - Max 63 properties', () {
       loadGame('assets/games/adventureland.z5');
-      expect(Z.engine.version, equals(ZMachineVersions.v5), reason: 'Should be Version 5');
+      expect(
+        Z.engine.version,
+        equals(ZMachineVersions.v5),
+        reason: 'Should be Version 5',
+      );
 
       // Should work for 1-63
       expect(GameObject.getPropertyDefault(1), isA<int>());
@@ -62,11 +70,19 @@ void main() {
 
     test('V8 (Anchorhead) - Max 63 properties', () {
       loadGame('assets/games/anchor.z8');
-      expect(Z.engine.version, equals(ZMachineVersions.v8), reason: 'Should be Version 8');
+      expect(
+        Z.engine.version,
+        equals(ZMachineVersions.v8),
+        reason: 'Should be Version 8',
+      );
 
       // Should work for 1-63
       expect(GameObject.getPropertyDefault(1), isA<int>());
-      expect(GameObject.getPropertyDefault(35), isA<int>(), reason: 'Property 35 should be valid in V8');
+      expect(
+        GameObject.getPropertyDefault(35),
+        isA<int>(),
+        reason: 'Property 35 should be valid in V8',
+      );
       expect(GameObject.getPropertyDefault(63), isA<int>());
 
       // Should throw for 64+
