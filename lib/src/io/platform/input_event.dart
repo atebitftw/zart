fc242a981176d2e9652bb416fb443ac50a8800ba
John Evans <prujohn@gmail.com>
2025-12-23 09:56:53 -0800

    doc comments

--- lib/src/io/platform/input_event.dart
+++ lib/src/io/platform/input_event.dart
@@ -75,22 +75,14 @@ class InputEvent {
     : this._(type: InputEventType.character, character: char, keyCode: keyCode);
 
   /// Create a special key event (arrows, function keys, etc).
-  const InputEvent.specialKey(int keyCode)
-    : this._(type: InputEventType.character, character: '', keyCode: keyCode);
+  const InputEvent.specialKey(int keyCode) : this._(type: InputEventType.character, character: '', keyCode: keyCode);
 
   /// Create a mouse click event.
   const InputEvent.mouseClick(int x, int y, MouseButton button, {int? windowId})
-    : this._(
-        type: InputEventType.mouse,
-        x: x,
-        y: y,
-        button: button,
-        windowId: windowId,
-      );
+    : this._(type: InputEventType.mouse, x: x, y: y, button: button, windowId: windowId);
 
   /// Create a timer/timeout event.
-  const InputEvent.timeout()
-    : this._(type: InputEventType.timer, isTimeout: true);
+  const InputEvent.timeout() : this._(type: InputEventType.timer, isTimeout: true);
 
   /// Create a resize event.
   const InputEvent.resize(int width, int height)
@@ -100,10 +92,7 @@ class InputEvent {
   const InputEvent.none() : this._(type: InputEventType.none);
 
   /// True if this is a printable character (not a special key).
-  bool get isPrintable =>
-      type == InputEventType.character &&
-      character != null &&
-      character!.isNotEmpty;
+  bool get isPrintable => type == InputEventType.character && character != null && character!.isNotEmpty;
 
   @override
   String toString() {
@@ -129,40 +118,97 @@ class InputEvent {
 /// These are the standard key codes used by both VMs.
 abstract class SpecialKeys {
   // Control keys
+  /// Delete key
   static const int delete = 8;
+
+  /// Tab key
   static const int tab = 9;
+
+  /// Enter key
   static const int enter = 13;
+
+  /// Escape key
   static const int escape = 27;
 
   // Arrow keys (Z-machine codes)
+  /// Arrow up key
   static const int arrowUp = 129;
+
+  /// Arrow down key
   static const int arrowDown = 130;
+
+  /// Arrow left key
   static const int arrowLeft = 131;
+
+  /// Arrow right key
   static const int arrowRight = 132;
 
   // Function keys
+  /// F1 key
   static const int f1 = 133;
+
+  /// F2 key
   static const int f2 = 134;
+
+  /// F3 key
   static const int f3 = 135;
+
+  /// F4 key
+  /// F4 key
   static const int f4 = 136;
+
+  /// F5 key
   static const int f5 = 137;
+
+  /// F6 key
   static const int f6 = 138;
+
+  /// F7 key
   static const int f7 = 139;
+
+  /// F8 key
   static const int f8 = 140;
+
+  /// F9 key
   static const int f9 = 141;
+
+  /// F10 key
   static const int f10 = 142;
+
+  /// F11 key
   static const int f11 = 143;
+
+  /// F12 key
   static const int f12 = 144;
 
   // Keypad (for Z-machine v6+)
+  /// Keypad 0 key
   static const int keypad0 = 145;
+
+  /// Keypad 1 key
   static const int keypad1 = 146;
+
+  /// Keypad 2 key
   static const int keypad2 = 147;
+
+  /// Keypad 3 key
   static const int keypad3 = 148;
+
+  /// Keypad 4 key
   static const int keypad4 = 149;
+
+  /// Keypad 5 key
   static const int keypad5 = 150;
+
+  /// Keypad 6 key
   static const int keypad6 = 151;
+
+  /// Keypad 7 key
   static const int keypad7 = 152;
+
+  /// Keypad 8 key
   static const int keypad8 = 153;
+
+  /// Keypad 9 key
   static const int keypad9 = 154;
 }
